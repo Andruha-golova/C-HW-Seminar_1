5203502442101117ba3e0b39b1155c46149d0e72
Andrey <andrey.ra4kov1988@gmail.com>
2023-02-01 23:13:24 +0300

    Task6

---
+++ Task_2/Task_4/Task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Enter number:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 == 0 )
+{
+    System.Console.WriteLine("even number");
+}
+else if(number % 2 ==1)
+{
+    System.Console.WriteLine("not even number");
+}
