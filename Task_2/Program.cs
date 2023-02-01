abd35cc6ba2b35b035c3bcbf31661bdf2e0253b2
Andrey <andrey.ra4kov1988@gmail.com>
2023-02-01 23:31:21 +0300

    Task2

---
+++ Task_2/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.WriteLine("enter number A:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("enter number B:");  
+int numberB = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (numberA > numberB) max = numberA;
+if (numberB > numberA) max = numberB;
+System.Console.WriteLine($"The maximum number is:{max}");
